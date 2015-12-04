eae35d338548eaec56ffba5563a32832d834d91a
David Kraus <dkra@num42.de>
2015-12-04 13:45:32 +0100

    cosmetics

--- OAuth2+TVOS/OAuth2+TVOS.swift
+++ OAuth2+TVOS/OAuth2+TVOS.swift
@@ -1,16 +1,18 @@
 extension OAuth2
 {
 	
+    // no webview or webbrowser available on tvOS
+    
 	public final func openAuthorizeURLInBrowser(params: [String: String]? = nil) -> Bool {
-		fatalError("Not yet implemented")
+		fatalError("Not implemented")
 	}
 	
 	public func authorizeEmbeddedWith(config: OAuth2AuthConfig, params: [String: String]? = nil, autoDismiss: Bool = true) -> Bool {
-        fatalError("no supported on TVOS!")
+        fatalError("Not implemented")
 	}
 	
 	public func authorizeEmbeddedFrom(controller: UIViewController, params: [String: String]?) -> AnyObject {
-		fatalError("no supported on TVOS!")
+		fatalError("Not implemented")
 	}
 }
 
