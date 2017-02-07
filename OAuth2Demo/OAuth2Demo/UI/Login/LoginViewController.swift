2e2ed1498e506b495be289944518dd2c0f4e0fd1
Amaury DAVID <amaury@tymate.com>
2017-02-07 17:01:33 +0100

    Added demo project

---
+++ OAuth2Demo/OAuth2Demo/UI/Login/LoginViewController.swift
@@ -0,0 +1,11 @@
+//
+// Created by tymate on 07/02/2017.
+// Copyright (c) 2017 tymate. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController : UIViewController {
+
+}
---
+++ OAuth2Demo/OAuth2Demo/UI/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  OAuth2Demo
+//
+//  Created by tymate on 07/02/2017.
+//  Copyright © 2017 tymate. All rights reserved.
+//
+
+import UIKit
+import OAuth2
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+}
+
