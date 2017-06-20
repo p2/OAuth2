1ba6e8c39c54148a0a48aa532491e900af1d1447
Robert Barclay <rbarclay@jive.com>
2017-06-20 15:03:13 -0600

    wrapped documentation lines at column 120 per PR request.

--- Sources/iOS/OAuth2Authorizer+iOS.swift
+++ Sources/iOS/OAuth2Authorizer+iOS.swift
@@ -55,9 +55,10 @@ open class OAuth2Authorizer: OAuth2AuthorizerUI {
 	*/
 	public func openAuthorizeURLInBrowser(_ url: URL) throws {
 		
-		// By asking for the shared instance method by using the "value for key" method on UIApplication, we are able to bypass the Swfit compilation 
-		// restriction that blocks the library from being compiled for an extension when directly referencing it. We do it as an optional so in the 
-		// advent of this method being called, like in an extension, we handle it as though its not supported.
+		// By asking for the shared instance method by using the "value for key" method on UIApplication, we are able to
+		// bypass the Swfit compilation restriction that blocks the library from being compiled for an extension when 
+		// directly referencing it. We do it as an optional so in the advent of this method being called, like in an 
+		// extension, we handle it as though its not supported.
 		guard let application = UIApplication.value(forKey: "sharedApplication") as? UIApplication else {
 			throw OAuth2Error.unableToOpenAuthorizeURL
 		}
