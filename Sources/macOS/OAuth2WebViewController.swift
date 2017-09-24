45ae561ce431a4e67605e02e31abd24935b84449
Paul Wilkinson <paulw@wilko.me>
2017-09-24 21:48:23 +1000

    Indent

--- Sources/macOS/OAuth2WebViewController.swift
+++ Sources/macOS/OAuth2WebViewController.swift
@@ -282,8 +282,8 @@ public class OAuth2WebViewController: NSViewController, WKNavigationDelegate, NS
 	// MARK: - Window Delegate
 
     public func windowShouldClose(_ sender: NSWindow) -> Bool {
-        onWillCancel?()
-        return false
+		onWillCancel?()
+		return false
 	}
 }
 
