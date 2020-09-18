41da26c5abd12f7e826ea82a0f0b5238e8162823
Mikolaj Lukasik <mlukasik@gmail.com>
2020-09-18 19:20:05 +0100

    #350
    Updated the iOS deployment target to 9.0+ to silence the warning in Xcode 12

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ import PackageDescription
 let package = Package(
 	name: "OAuth2",
 	platforms: [
-		.macOS(.v10_11), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)
+		.macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v3)
 	],
 	products: [
 		.library(name: "OAuth2", targets: ["Base", "Flows", "DataLoader"]),
